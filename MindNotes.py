b5ff0e5ccddfd14ea0ce00633a8a5c1c63fca56c
Valerii Syroiezhin <86792918+syroiezhin@users.noreply.github.com>
2022-08-19 23:51:05 +0300

    implemented a database based on a json file

--- MindNotes.py
+++ MindNotes.py
@@ -1,6 +1,9 @@
+from pyperclip import copy
 from json import load, dump
+from os import system, popen
 from rumps import debug_mode
 from rumps import App, MenuItem
+from googletrans import Translator
 from rumps import quit_application, alert
 
 VERSION = '2022.08.19'
@@ -8,15 +11,31 @@ VERSION = '2022.08.19'
 '''
 It is necessary to implement:
 0) C̶r̶e̶a̶t̶e̶ a̶n̶ a̶p̶p̶r̶o̶x̶i̶m̶a̶t̶e̶ i̶n̶t̶e̶r̶f̶a̶c̶e̶ o̶f̶ t̶h̶e̶ i̶n̶t̶e̶n̶d̶e̶d̶ p̶r̶o̶g̶r̶a̶m̶
-1) Implement saving data to a JSON file and the ability to update programs when changes are made
+1) I̶m̶p̶l̶e̶m̶e̶n̶t̶ s̶a̶v̶i̶n̶g̶ d̶a̶t̶a̶ t̶o̶ a̶ J̶S̶O̶N̶ f̶i̶l̶e̶ a̶n̶d̶ t̶h̶e̶ a̶b̶i̶l̶i̶t̶y̶ t̶o̶ u̶p̶d̶a̶t̶e̶ p̶r̶o̶g̶r̶a̶m̶s̶ w̶h̶e̶n̶ c̶h̶a̶n̶g̶e̶s̶ a̶r̶e̶ m̶a̶d̶e̶
 2) Add buttons: hide desktop; hide the name of the program; find global and local IP address
 3) Add the ability to select a language
 4) Create a form to create new notes
 5) Add the ability to delete a note from the database
 6) Implement icon change "day and night"
+7) Сollect app file
 '''
 
-def Mode(Incognito): Incognito.state = not Incognito.state
+def Update(): system('bash rerun.sh')
+
+def Save(denomination, variable):
+    with open('settings.json', 'rt') as r: content = load(r)
+    content[denomination] = variable.state
+    with open('settings.json', 'wt') as w: dump(content, w, sort_keys=True, indent=2)
+
+def Header(Title):
+    Title.state = not Title.state
+    Save( [name for name, value in locals().items() if value is Title][0] , Title )
+    Update()
+
+def Mode(Incognito): 
+    Incognito.state = not Incognito.state
+    Save( [name for name, value in locals().items() if value is Incognito][0] , Incognito )
+    Update()
 
 def Add(Add):   alert(title='Сreate Note', message='...', ok='Keep', cancel='Cancel')
 
@@ -30,8 +49,10 @@ def menu():
     Сreate = MenuItem("Create", key="c", callback = Add)
     Invisible = MenuItem("Incognito", key="i", callback = Mode)
     Invisible.state = content['Incognito']
-    Exit = MenuItem("Exit", key="q", callback = Quit)
+    Title = MenuItem("Hide Header", key="h", callback = Header)
+    Title.state = content['Title']
+    Exit = MenuItem("Quit", key="q", callback = Quit)
 
-    return [ Сreate, None, [ "Settings", [ Invisible, [ "Language", Accents ], None, " "+VERSION, None, Exit ] ], None, [ "Notes", Note ] ]
+    return [ Сreate, None, [ "Settings", [ Invisible, [ "Language", Accents ], None, Title, None, " "+VERSION, None, Exit ] ], None, [ "Notes", Note ] ]
 
-if __name__ == "__main__": App('Mind', icon = 'MN.png', title = 'MindNotes' if load(open('settings.json', 'rt'))['Title'] == 0 else None, menu = menu(), quit_button=None).run()
+if __name__ == "__main__": App('MindNotes', icon = 'MN.png', title = 'MindNotes' if load(open('settings.json', 'rt'))['Title'] == 0 else None, menu = menu(), quit_button=None).run()
