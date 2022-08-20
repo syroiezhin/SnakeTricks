0de831bdba7f8571610534b79f3550ea01697e0e
Valerii Syroiezhin <86792918+syroiezhin@users.noreply.github.com>
2022-08-21 00:06:05 +0300

    correction

--- MindNotes.py
+++ MindNotes.py
@@ -6,7 +6,7 @@ from googletrans import Translator # pip install googletrans==4.0.0-rc1
 from rumps import App, MenuItem, Window
 from rumps import quit_application, alert
 
-VERSION = '2022.08.20d'
+VERSION = '2022.08.20e'
 
 '''
 It is necessary to implement:
@@ -17,14 +17,14 @@ It is necessary to implement:
 4) C̶r̶e̶a̶t̶e̶ a̶ f̶o̶r̶m̶ t̶o̶ c̶r̶e̶a̶t̶e̶ n̶e̶w̶ n̶o̶t̶e̶s̶
 5) Add the ability to delete a note from the database
 6) Implement icon change "day and night"
-7) Сollect app file
+7) C̶o̶l̶l̶e̶c̶t̶ a̶p̶p̶ f̶i̶l̶e̶
 '''
 
 def Update(): system('bash rerun.sh')
 
 def Save(denomination, variable):
     with open('settings.json', 'rt') as r: content = load(r)
-    content[denomination] = variable.state
+    content['Keys'][denomination] = variable.state
     with open('settings.json', 'wt') as w: dump(content, w, sort_keys=True, indent=2)
 
 def Desktop(HideDesktop):
@@ -49,9 +49,9 @@ def Hint(Hints):
 
 def Add(Add):
     with open('settings.json', 'rt') as r: content = load(r)
-    name_site = Window(title='Сreate Note (1/3)', default_text='Google' if content['Hints'] == 0 else "", message='Set note title', ok='Next', cancel='Cancel', dimensions=(200, 25)).run()
+    name_site = Window(title='Сreate Note (1/3)', default_text='Google' if content['Keys']['Hints'] == 0 else "", message='Set note title', ok='Next', cancel='Cancel', dimensions=(200, 25)).run()
     if name_site.clicked == 1:
-        url_site = Window(title='Сreate Note (2/3)', default_text='https://www.google.com' if content['Hints'] == 0 else "", message='Paste the copied link to the source', ok='Next', cancel='Cancel', dimensions=(200, 25)).run()
+        url_site = Window(title='Сreate Note (2/3)', default_text='https://www.google.com' if content['Keys']['Hints'] == 0 else "", message='Paste the copied link to the source', ok='Next', cancel='Cancel', dimensions=(200, 25)).run()
         if url_site.clicked == 1:
             if alert(title='Сreate Note (3/3)', message='Click \'Keep\' to save to database', ok='Keep', cancel='Cancel') == 1: 
                 with open('settings.json', 'rt') as r: content = load(r)
@@ -72,13 +72,13 @@ def menu():
     interface = content['Choose']['Menu']
     Сreate = MenuItem(interface[0], key="c", callback = Add)
     Invisible = MenuItem(interface[3], key="i", callback = Mode)
-    Invisible.state = content['Incognito']
+    Invisible.state = content['Keys']['Incognito']
     HideDesktop = MenuItem(interface[4], key="d", callback = Desktop)
-    HideDesktop.state = content['HideDesktop']
+    HideDesktop.state = content['Keys']['HideDesktop']
     Hints = MenuItem(interface[5], key="h", callback = Hint)
-    Hints.state = content['Hints']
+    Hints.state = content['Keys']['Hints']
     Title = MenuItem(interface[6], key="n", callback = Header)
-    Title.state = content['Title']
+    Title.state = content['Keys']['Title']
     GlobalIP = MenuItem(interface[7], key="g", callback = Global)
     LocalIP = MenuItem(interface[8], key="l", callback = Local)
     Exit = MenuItem(interface[10].capitalize(), key="q", callback = Quit)
@@ -95,9 +95,11 @@ def menu():
     for id,value in enumerate(content['Notes']['WebSite']):
         def Notes(Note):
             id = content['Notes']['WebSite'].index(str(Note).split('\'', 1)[1].split('\'')[0])
-            system(f"open -a Google\ Chrome --new --args {content['Notes']['URL'][id]}") if content['Incognito'] == 0 else system(f"open -a Google\ Chrome --new --args -incognito {content['Notes']['URL'][id]}")
+            system(f"open -a Google\ Chrome --new --args {content['Notes']['URL'][id]}") if content['Keys']['Incognito'] == 0 else system(f"open -a Google\ Chrome --new --args -incognito {content['Notes']['URL'][id]}")
         Note.append( MenuItem(value, key=str(id+1) if id<9 else None, callback = Notes) )
 
     return [ Сreate, None, [ interface[1].capitalize(), [ [ interface[2], Accents ], None, Invisible, None, HideDesktop, Hints, Title, None, GlobalIP, LocalIP, None, interface[9].capitalize()+" "+VERSION, None, Exit ] ], None, [ interface[11].capitalize(), Note ] ]
 
-if __name__ == "__main__": App('MindNotes', icon = 'MN.png', title = 'MindNotes' if load(open('settings.json', 'rt'))['Title'] == 0 else None, menu = menu(), quit_button=None).run()
+if __name__ == "__main__": App('MindNotes', icon = 'MN.png', title = 'MindNotes' if load(open('settings.json', 'rt'))['Keys']['Title'] == 0 else None, menu = menu(), quit_button=None).run()
+
+# com.apple.finder AppleShowAllFiles TRUE/FALSE
